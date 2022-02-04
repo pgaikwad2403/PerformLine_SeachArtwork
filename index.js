f3a9914b6ce7e0b44d7a24a5ae3b1200877275bb
Pooja Gaikwad <pooja.ngaikwad@gmail.com>
2022-02-03 20:52:35 -0500

    Final changes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const searchArtworks = () => {
     const searchValue = document.getElementById("searchInput").value;
     const checkboxValue = document.getElementById("isPublicDomain").checked;
@@ -34,52 +33,57 @@ const searchArtworks = () => {
 
     /* Get data for each object ID and store in an array */
     getObjectIDs().then(async (objIDArray) => {
-        let topFiveResult = []; // array to store data for first 5 objects
+        if (objIDArray === null) {
+            alert("No results for for this search");
+        } else {
+            let topFiveResult = []; // array to store data for first 5 objects
 
-        for (let i = 0; i < objIDArray.length; i++) {
-            if (topFiveResult.length === 5) {
-                break; // end loop if array is populated with 5 results
+            for (let i = 0; i < objIDArray.length; i++) {
+                if (topFiveResult.length === 5) {
+                    break; // end loop if array is populated with 5 results
+                }
+                // url to retrieve data on an object
+                let url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objIDArray[i]}`;
+                let response = await fetch(url);
+                let data = await response.json();
+
+                // check if user checked public domain checkbox
+                if (checkboxValue && !data.isPublicDomain) {
+                    continue;
+                }
+
+                topFiveResult.push(data); //push object data in array
             }
-            // url to retrieve data on an object
-            let url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objIDArray[i]}`;
-            let response = await fetch(url);
-            let data = await response.json();
 
-            // check if user checked public domain checkbox
-            if (checkboxValue && !data.isPublicDomain) {
-                continue;
-            }
+            // loop through array to display results
+            topFiveResult.map((object) => {
+                var result = document.createElement("ul"); // create list for each result
+                result.classList.add("result"); //add classname to list
 
-            topFiveResult.push(data); //push object data in array
+                // create list items for result title, image, artist and department
+                var title = document.createElement("li");
+                var image = document.createElement("img");
+                image.src = object.primaryImage;
+                var artistName = document.createElement("li");
+                var department = document.createElement("li");
+
+                title.appendChild(
+                    document.createTextNode(`Title: ${object.title}`)
+                );
+                artistName.appendChild(
+                    document.createTextNode(
+                        `Artist: ${object.artistDisplayName}`
+                    )
+                );
+                department.appendChild(
+                    document.createTextNode(`Department: ${object.department}`)
+                );
+                // append list items to the list created
+                result.append(title, image, artistName, department);
+
+                // append the list of results to div
+                document.querySelector("#results").appendChild(result);
+            });
         }
-        console.log(topFiveResult);
-
-        // loop through array to display results
-        topFiveResult.map((object) => {
-            var result = document.createElement("ul"); // create list for each result
-            result.classList.add("result"); //add classname to list
-
-            // create list items for result title, image, artist and department
-            var title = document.createElement("li");
-            var image = document.createElement("img");
-            image.src = object.primaryImage;
-            var artistName = document.createElement("li");
-            var department = document.createElement("li");
-
-            title.appendChild(
-                document.createTextNode(`Title: ${object.title}`)
-            );
-            artistName.appendChild(
-                document.createTextNode(`Artist: ${object.artistDisplayName}`)
-            );
-            department.appendChild(
-                document.createTextNode(`Department: ${object.department}`)
-            );
-            // append list items to the list created
-            result.append(title, image, artistName, department);
-            
-            // append the list of results to div
-            document.querySelector("#results").appendChild(result);
-        });
     });
 };
